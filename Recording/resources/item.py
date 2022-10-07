1026c729e66968b5e4069e9f070bd17fe9079664
ASNTHEGREAT <aditya.nittala5@gmail.com>
2022-10-08 01:35:22 +0530

    added different files for different functions and clear main

---
+++ Recording/resources/item.py
@@ -0,0 +1,42 @@
+import uuid
+from flask import request
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from db import items
+
+
+blp = Blueprint("Items", __name__, description="Operations on items")
+
+@blp.route("/item/<string:item_id>")
+class Item(MethodView):
+    def get(self, item_id):
+        try:
+            return items[item_id]
+        except KeyError:
+            abort(404, message="Item not found.")
+        
+    def delete(self, item_id):
+        try:
+            del items[item_id]
+            return {"message": "Item deleted."}
+        except KeyError:
+            abort(404, message="Item not found.")
+    
+    def put(self, item_id):
+        item_data = request.get_json()
+        # There's  more validation to do here!
+        # Like making sure price is a number, and also both items are optional
+        # You should also prevent keys that aren't 'price' or 'name' to be passed
+        # Difficult to do with an if statement...
+        if "price" not in item_data or "name" not in item_data:
+            abort(
+                400,
+                message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.",
+            )
+        try:
+            item = items[item_id]
+            item |= item_data
+
+            return item
+        except KeyError:
+            abort(404, message="Item not found.")
