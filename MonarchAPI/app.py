15f5f1152a33997043b5bdb55ce4d6634331aa7d
ASNTHEGREAT <aditya.nittala5@gmail.com>
2022-10-17 00:24:04 +0530

    adding some sql files to recording and Adding initial commit for MonarchAPI

---
+++ MonarchAPI/app.py
@@ -0,0 +1,25 @@
+from crypt import methods
+import flask
+from flask import request
+import pandas as pd
+from datetime import datetime as dt
+
+data = pd.read_csv('data.csv', names=['s','e','m']).set_index('m')
+
+series = pd.Series(index=range(data.s.min(), dt.now().year))    #This creates an empty padas series, starting from first year up until this year.
+for m in data.index:
+    series.loc[data.loc[m].s:data.loc[m].e] = m #Each year when the monarch is raining the year is going to be there.
+
+
+
+
+app = flask.Flask(__name__)
+
+app.route('/', methods=['GET'])
+def home():
+    year = int(request.args['year'])
+    try:
+        return series.loc[year]
+    except KeyError:
+        return f'Invalid Input ({series.index.min()} - {series.index.max()})'
+
--- Recording/resources/item.py
+++ Recording/resources/item.py
@@ -3,6 +3,10 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError
+
+from db import db
+from models import ItemModel
 from schemas import ItemSchema, ItemUpdateSchema
 
 
@@ -17,18 +21,13 @@ class ItemList(MethodView):
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
     def post(self, item_data):
-        item_data = request.get_json()
+        item = ItemModel(**item_data)
 
-        for item in items.values():
-            if(
-                item_data["name"] == item["name"]
-                and item_data["store_id"] == item["store_id"]
-            ):
-                abort(400, message=f"Item already exists.")
-        
-        item_id = uuid.uuid4().hex
-        item = {**item_data, "id": item_id}
-        items[item_id] = item
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occured while inserting the item.")
 
         return item
 
