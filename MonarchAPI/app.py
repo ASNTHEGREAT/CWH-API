950774e840a9e79333433c1d35732a2091bf20d4
ASNTHEGREAT <aditya.nittala5@gmail.com>
2022-10-17 00:52:37 +0530

    current year bug resolved

--- MonarchAPI/app.py
+++ MonarchAPI/app.py
@@ -6,7 +6,7 @@ from datetime import datetime as dt
 
 data = pd.read_csv('data.csv', names=['s','e','m']).set_index('m')
 
-series = pd.Series(index=range(data.s.min(), dt.now().year))    #This creates an empty padas series, starting from first year up until this year.
+series = pd.Series(index=range(data.s.min(), dt.now().year + 1))    #This creates an empty padas series, starting from first year up until this year.
 for m in data.index:
     series.loc[data.loc[m].s:data.loc[m].e] = m #Each year when the monarch is raining the year is going to be there.
 
